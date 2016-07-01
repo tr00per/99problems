7f67b1d6c0383c10e512bd64a3d3262041e8a027
Artur Czajka <arturczajka@gmail.com>
2016-07-01 19:26:48 +0200

    Up to 23rd problem, 24th WIP

--- haskell/Lists.hs
+++ haskell/Lists.hs
@@ -1,7 +1,15 @@
+-- Solutions to "99 Prolog Problems" (c) 2016 Artur Czajka. 2-clause BSD license applies.
+
+-- ScopedTypeVariables is needed to explicity write type signature for `draw` in `problem23`
+{-# LANGUAGE ScopedTypeVariables #-}
 module Lists where
 
-import Data.List
-import Control.Arrow
+import           Control.Arrow
+import           Control.Monad
+import           Control.Monad.Trans
+import           Control.Monad.Trans.State
+import           Data.List
+import           System.Random
 
 problem01 :: [a] -> a
 problem01 = last
@@ -52,11 +60,68 @@ problem15 n = concatMap (replicate n)
 problem16 :: Int -> [a] -> [a]
 problem16 _ [] = []
 problem16 n xs = removeNth xs
-    where removeNth = uncurry (++) . second (problem16 n . safeTail) . splitAt (n-1)
-          safeTail as = if null as then as else tail as
+    where removeNth = uncurry (++) . second (problem16 n . tailOrNull) . splitAt (n-1)
+          tailOrNull as = if null as then as else tail as
 
 problem17 :: Int -> [a] -> ([a], [a])
 problem17 = splitAt
 
 problem18 :: Int -> Int -> [a] -> [a]
-problem18 from to = take (to-from+1) . drop (from-1)
+problem18 from to = take (to-from+1) . drop from
+
+problem19 :: Int -> [a] -> [a]
+problem19 n = uncurry (flip (++)) . splitAt n
+
+problem20 :: Int -> [a] -> (a, [a])
+problem20 n xs = let (as,bs) = splitAt n xs
+                 in (head bs, as ++ tail bs)
+
+problem21 :: Int -> a -> [a] -> [a]
+problem21 0 a xs = a:xs
+problem21 _ a [] = [a]
+problem21 n a (x:xs) = x : problem21 (n-1) a xs
+
+problem22 :: Int -> Int -> [Int]
+problem22 = enumFromTo
+
+problem23 :: forall a . Int -> [a] -> State StdGen [a]
+problem23 n collection = mapM (draw $ length collection) [1..n]
+    where draw :: Int -> Int -> State StdGen a
+          draw limit _ = do
+              (x, newGen) <- next <$> get
+              put newGen
+              let idx = x `mod` limit
+              return (collection !! idx)
+
+runProblem23 :: Int -> [a] -> IO [a]
+runProblem23 n collection = evalState (problem23 n collection) <$> newStdGen
+
+problem24 :: Int -> Int -> StateT [Int] (State StdGen) ()
+problem24 n limit = do
+
+    result <- get
+    r <- lift randomize
+    when (r `notElem` result) $ put (r:result)
+
+    return ()
+    where
+        randomize :: State StdGen Int
+        randomize = do
+            (x, newGen) <- next <$> get
+            put newGen
+            return (x `mod` limit + 1)
+
+runProblem24 :: Int -> Int -> IO [Int]
+runProblem24 n limit = evalState (execStateT (problem24 n limit) []) <$> newStdGen
+
+-- non-unique values
+problem24' :: Int -> Int -> State StdGen [Int]
+problem24' n limit = mapM randomize [1..n]
+    where randomize :: Int -> State StdGen Int
+          randomize _ = do
+              (x, newGen) <- next <$> get
+              put newGen
+              return (x `mod` limit + 1)
+
+runProblem24' :: Int -> Int -> IO [Int]
+runProblem24' n limit = evalState (problem24' n limit) <$> newStdGen
