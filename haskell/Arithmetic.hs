e790bd4458179dcff28a1bc8b3a8faadcdba6939
Artur Czajka <arturczajka@gmail.com>
2016-07-02 15:03:32 +0200

    Up to 6th

--- haskell/Arithmetic.hs
+++ haskell/Arithmetic.hs
@@ -3,6 +3,7 @@
 module Arithmetic where
 
 import           Control.Arrow
+import           Control.Monad
 import           Data.List
 import           Data.Numbers.Primes
 
@@ -16,11 +17,22 @@ problem03 :: Integral a => a -> [(a, Int)]
 problem03 =  map (head &&& length) . group . primeFactors
 
 problem04 :: (Integral a, Ord a) => a -> a -> [a]
-problem04 min max = takeWhile (<max) $ dropWhile (<min) primes
+problem04 low high = takeWhile (<high) $ dropWhile (<low) primes
 
 problem05 :: Int -> (Int, Int)
 problem05 target = head $ filter (\(x,y) -> x+y == target) pairs
     where
         pairs :: [(Int, Int)]
-        pairs = undefined
+        pairs = [(x,y) | x <- somePrimes, y <- somePrimes]
         somePrimes = takeWhile (<target) primes
+
+problem06 :: Int -> Int -> [(Int, Int)]
+problem06 low high = let low' = max low 4 in map problem05 [low',low'+2..high]
+
+testProblem06 limit = forM_ (filter bothBigger $ problem06 2 limit) $ \(x,y) ->
+    putStrLn $ show (x+y) ++ " = " ++ show x ++ " + " ++ show y
+    where
+        bothBigger :: (Int, Int) -> Bool
+        bothBigger pair = case ((>50) *** (>50)) pair of
+            (True, True) -> True
+            _ -> False
