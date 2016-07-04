dbaf0a9ef2c4991ce5b096125547914012066040
Artur Czajka <arturczajka@gmail.com>
2016-07-04 09:41:35 +0200

    5th WIP

--- haskell/Logic.hs
+++ haskell/Logic.hs
@@ -3,10 +3,9 @@
 module Logic where
 
 import           Control.Arrow
-import           Data.List     (intercalate)
 import           Data.Map      ((!))
 import qualified Data.Map      as M
-import           Prelude       hiding (and, not, or)
+import           Prelude       hiding (Left, Right, and, not, or)
 import qualified Prelude       as P
 
 type Identifier = Char
@@ -61,12 +60,15 @@ printTable :: [String] -> IO ()
 printTable = putStrLn . unlines
 
 -- and(A,or(A,B))
+problem01 :: IO ()
 problem01 = printTable $ table ['A', 'B'] $ and (var 'A') (or (var 'A') (var 'B'))
 
 -- A and (A or not B)
+problem02 :: IO ()
 problem02 = printTable $ table ['A', 'B'] $ var 'A' `and` (var 'A' `or` not (var 'B'))
 
 -- A and (B or C) equ A and B or A and C
+problem03 :: IO ()
 problem03 = printTable $ table ['A', 'B', 'C'] $
     (var 'A' `and` (var 'B' `or` var 'C')) `equ` ((var 'A' `and` var 'B') `or` (var 'A' `and` var 'C'))
 
@@ -78,3 +80,27 @@ problem04 len target = target `elem` grays len
             h <- ['0', '1']
             t <- grays (n-1)
             return (h : t)
+
+data CodeTree a = Node [a] (CodeTree a) (CodeTree a) | Left a | Right a
+
+contains :: Eq a => CodeTree a -> a
+contains = undefined
+
+buildFromFreqMap :: M.Map a Int -> CodeTree a
+buildFromFreqMap = undefined
+
+problem05 :: M.Map Char Int -> M.Map Char String
+problem05 frequencies = fmap (\(ch, _) -> (ch, findInCodeTree ch)) frequencies
+    where
+        codeTree :: CodeTree Char
+        codeTree = buildFromFreqMap frequencies
+
+        findInCodeTree :: Char -> String
+        findInCodeTree ch = find ch codeTree
+            where find (Node l r) =
+                      if l `contains` ch then find ch l else find ch r
+                  find (Left c) =
+                      if ch == c then "" else error ("Unknown code for " ++ show c)
+
+runProblem05 :: IO ()
+runProblem05 = print $ problem05 $ M.fromList [('a', 45),('b', 13),('c', 12),('d', 16),('e', 9),('f', 5)]
